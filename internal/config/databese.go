1698582fdc3d4ead6652a90f7cdb82413db585ec
ZedGred <pebiyanfirmansyah22@gmail.com>
2025-06-29 16:00:53 +0700

    fix: bug

--- internal/config/databese.go
+++ internal/config/databese.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -12,12 +13,20 @@ import (
 
 // Create Connection
 func NewMongo(config *viper.Viper, log *logrus.Logger) *mongo.Client {
-	MongoDB := config.GetString("databese.mongo.url")
+	fmt.Println("aman")
+	MongoDB := config.GetString("database.mongo.url")
+	if MongoDB == "" {
+		log.Fatal("MongoDB URL is not set")
+	}
+
+	fmt.Println(MongoDB)
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	fmt.Println("aman")
 	if err != nil {
-		log.Fatalf("Failed to Connect Databese : %v",err)
+		log.Errorf("Failed to Connect Databese : %v",err)
 	}
+	fmt.Println("aman")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
